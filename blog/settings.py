86b0fa4930918a0c3f364087829e2ca85479f329
FESTUS <adelekanf7@gmail.com>
2026-03-11 04:25:42 +0100

    Secure initial commit: Postgres config and env variables

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q(gw2a9u0p&-2x%fn0lz758kn0f2e^6+oh26o5mv%=qc#r+e!k'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
